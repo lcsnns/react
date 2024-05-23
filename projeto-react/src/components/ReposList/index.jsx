e78aa4f80ba2e6c9747aa3231f32dd2e599b42b5
lucas nunes <145053610+lcsnns@users.noreply.github.com>
2024-05-23 20:12:21 -0300

    duvida de react

--- projeto-react/src/components/ReposList/index.jsx
+++ projeto-react/src/components/ReposList/index.jsx
@@ -1,29 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ReposList = () => {
     const [repos, setRepos] = useState([]);
+    const [estaCarregando, setEstaCarregando] = useState(true);
 
     useEffect(() => {
         fetch('https://api.github.com/users/lcsnns/repos')
-        .then(res => res.json())
-        .then(resJson => {
-            setRepos(resJson);
-            console.log(resjson);
-        })
+            .then(res => res.json())
+            .then(resJson => {
+                setTimeout(() => {
+                    setEstaCarregando(false);
+                    setRepos(resJson);
+                }, 3000);
+            })
     }, []);
 
     return (
-        <ul>
-            {repos.map(repositorio => (
-                <li key={repositorio.id}>
-                    <b>Nome:</b> {repositorio.name}
-                    <b>Linguagem:</b> {repositorio.language}
-                    <a target='_blank' href={repositorio.html_url}>Visitar no GitHub</a>
-                </li>
-            ))}
-            <li>Repositório</li>
-        </ul>
-    )
+        <>
+            {setEstaCarregando && (
+            <h1>Carregando...</h1>
+            )}
+            <ul>
+                {repos.map(repositorio => (
+                    <li key={repositorio.id}>
+                        <b>Nome:</b> {repositorio.name} <br />
+                        <b>Linguagem:</b> {repositorio.language} <br />
+                        <a target='_blank' href={repositorio.html_url}>Visitar no GitHub</a> <br />
+                    </li>
+                ))}
+                <li>Repositório</li>
+            </ul>
+            </>
+            )
 }
 
 export default ReposList;
